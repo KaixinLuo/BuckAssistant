bf072a6efe16fdb4d0e3bb89de09ed3bbdc1c1f3
kaixinluo <704556595@qq.com>
2018-09-23 15:21:26 -0400

    "--init--"

--- BuckAD/toolbox.py
+++ BuckAD/toolbox.py
@@ -2,7 +2,7 @@ import json
 
 from watson_developer_cloud import DiscoveryV1
 from watson_developer_cloud import AssistantV1
- 
+
 bot = AssistantV1(
     version='2018-01-01',
     username='c7721aaf-1c20-4325-bb11-6cfe0cdb8967',
@@ -17,27 +17,23 @@ brain = DiscoveryV1(
     url='https://gateway.watsonplatform.net/discovery/api'
 )
 
- #= '48fca871-ad3b-417e-b222-077b13523ccb'
-#collection_id = '66d011eb-a6fe-4cc6-b5e9-1995284d34e3'
-def initialize():
-    pass
-
-def get_all_environment_ID():
-    return [e['environment_id'] for e in brain.list_environments()["environments"]]
-    
-def get_all_collection_ID(env_id):
-    return [c['collection_id'] for c in brain.list_collections(env_id)['collections']]
-
-def get_collection(e_id,c_id):
-    return None
+class Brain:
+    def __init__(self,kernel:DiscoveryV1):
+        self.kernel = kernel
+        self.currentEnvironment = kernel.list_environments(name = 'byod')['environments'][0]['environment_id']
+        self.collectionIndex = {e["name"]:e["collection_id"] for e in kernel.list_collections(environment_id=self.currentEnvironment)["collections"]}
 
 
-class BuckAss:
-    def __init__(self):
-        self.front = bot
-        self.back = brain
-        self.__current_env__ =get_all_environment_ID[1]
-        self.__current_collection__ = get_all_collection_ID()[0]
-    def get_the_most_likely_document(self,q):
-         response = brain.query(self.__current_env__,self.__current_collection__,natural_language_query=q)
-         return response['result'][0]['id']
+    def retrieve_first_doc_text(self,nl_query):
+        return {
+        name: self.kernel.query(self.currentEnvironment, col_id, natural_language_query=nl_query)["results"][0]["text"]
+        for (name, col_id) in self.collectionIndex.items()}
+
+    def retrieve_docs_by_confidence(self,query,key = "text"):
+        r = {name:self.kernel.query(self.currentEnvironment, col_id, natural_language_query=query)for (name,col_id) in self.collectionIndex.items()}
+        return {k:[(e["text"],e["result_metadata"]["score"]) for e in elem["results"]] for (k,elem) in r.items()}
+
+
+b = Brain(brain)
+
+print(b.retrieve_docs_by_confidence("deliang wang")["CourseInfo"])
