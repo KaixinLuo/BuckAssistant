c9544f11ceab1361c6ceed547227e7477b117d36
kaixinluo <704556595@qq.com>
2018-09-23 20:03:07 -0400

    Kaixin Luo

--- BuckAD/EmailModule.py
+++ BuckAD/EmailModule.py
@@ -2,11 +2,11 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def construct_email_advisor(question,student_name):
+def construct_email_query(question,student_name):
     email='Dear advisor, \n\nI hope you are doing well. I want to make appointment with you for {}, what is your best time or could you tell me about it via email?\n\nBest,\n{}'.format(question,student_name)
     return email
 
-def construct_email_professor(professor_name,question,student_name):
+def construct_email_appointment(professor_name,question,student_name):
     email='Dear Prof. {},\n\nI hope you are doing well. I want to make appointment with you for {}, what is your best time to hold this appointment?\n\nBest,\n{}'.format(professor_name,question,student_name)
     return email
 
