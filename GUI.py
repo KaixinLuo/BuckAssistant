99fca16358824160ebf6b3c6e19c7d6e226442df
kaixinluo <704556595@qq.com>
2018-10-24 18:26:12 -0400

    Kaixin

--- GUI.py
+++ GUI.py
@@ -1,13 +1,16 @@
 import sys
-
+from facerecogonition import *
+from conversation_processor import *
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication,QTextEdit,QGridLayout,QHBoxLayout
 from PyQt5.QtCore import Qt
 
 class Application(QWidget):
     def __init__(self):
         super().__init__()
-        self.user = "usr"
-        pass
+
+        face = read_face()
+        self.user = face_recognizer(face)
+        self.conversationHandler = Conversation_Processor()
         self.initUI()
     def initUI(self):
         self.sendButton = QPushButton("Send")
@@ -20,8 +23,7 @@ class Application(QWidget):
         self.msgArea = QTextEdit()
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
-        
-        
+
         self.grid.addWidget(self.msgArea,0,0)
         self.grid.addWidget(self.textToSend,1,0,6,1)
         self.grid.addLayout(buttonLayout,7,0)
@@ -34,17 +36,18 @@ class Application(QWidget):
         self.setGeometry(300, 300, 1000, 618)
         self.setWindowTitle('Buttons') 
         self.show()
-    def login():
-        pass
+
     def send_button_pressed(self):
         raw_text = self.textToSend.toPlainText()
         if raw_text != "":
             self.msgArea.insertPlainText(self.user+":\n    "+self.textToSend.toPlainText()+"\n")
+            reply = self.conversationHandler.process_message(raw_text)
+            self.msgArea.insertPlainText("Advisor:\n    "+reply+"\n")
 
         self.textToSend.clear()
     def voice_button_pressed(self):
         pass
-
+    
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
