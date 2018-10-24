30864f037907410dd7ebff88ee4223104a46f8bd
kaixinluo <704556595@qq.com>
2018-10-24 19:13:04 -0400

    Kaixin

--- GUI.py
+++ GUI.py
@@ -1,16 +1,16 @@
 import sys
 from facerecogonition import *
 from conversation_processor import *
+from user import User
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication,QTextEdit,QGridLayout,QHBoxLayout
 from PyQt5.QtCore import Qt
 
 class Application(QWidget):
     def __init__(self):
         super().__init__()
-
-        face = read_face()
-        self.user = face_recognizer(face)
+        self.isFristMsg = True
         self.conversationHandler = Conversation_Processor()
+        self.login()
         self.initUI()
     def initUI(self):
         self.sendButton = QPushButton("Send")
@@ -20,7 +20,7 @@ class Application(QWidget):
         buttonLayout.addWidget(self.voiceButton)
         buttonLayout.addWidget(self.sendButton)
         self.textToSend = QTextEdit()
-        self.msgArea = QTextEdit()
+        self.msgArea = QTextEdit("Welcome! "+self.user)
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
 
@@ -33,11 +33,16 @@ class Application(QWidget):
         
 
         self.setLayout(self.grid)
-        self.setGeometry(300, 300, 1000, 618)
-        self.setWindowTitle('Buttons') 
+        self.setGeometry(300, 300,  618,1000)
+        self.setWindowTitle('Vritual Advisor') 
+        
         self.show()
 
     def send_button_pressed(self):
+        if self.isFristMsg:
+            self.msgArea.clear()
+            self.isFristMsg = False
+        
         raw_text = self.textToSend.toPlainText()
         if raw_text != "":
             self.msgArea.insertPlainText(self.user+":\n    "+self.textToSend.toPlainText()+"\n")
@@ -47,6 +52,17 @@ class Application(QWidget):
         self.textToSend.clear()
     def voice_button_pressed(self):
         pass
+    def login(self):
+        face = read_face()
+        self.user = face_recognizer(face)
+        u=User()
+        f = open("email.txt", "r")
+        f1 = f.read().splitlines()
+        u.set_email_address(f1[0])
+        u.set_email_password(f1[1])
+        u.set_name(self.user)
+        self.conversationHandler.assign_user_object(u)
+
     
 if __name__ == '__main__':
 
